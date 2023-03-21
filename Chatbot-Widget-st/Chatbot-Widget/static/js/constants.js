a6bf18370ae5d8a749ddacb9c81d266ffc68bb2f
A <atul.aggrawal97@gmail.com>
2023-03-21 10:34:19 +0530

    Delete Chatbot-Widget-st/Chatbot-Widget directory

--- Chatbot-Widget-st/Chatbot-Widget/static/js/constants.js
+++
@@ -1,5 +0,0 @@
-const action_name = "action_hello_world";
-const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
-const sender_id = uuidv4();
-
-const rasa_server_url2 = "http://localhost:8000/api/messages/14/13/";
